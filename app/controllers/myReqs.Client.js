0341f8d66d862cf05cbaa5925b6f84c6c73d8abb
Gibryon Bhojraj <gibryonbhojraj@gmail.com>
2016-02-23 23:18:26 +0000

    Creating myRequests pages.

---
+++ app/controllers/myReqs.Client.js
@@ -0,0 +1,10 @@
+'use strict';
+
+(function(){
+    angular.module("myReqs", [])
+    .controller("mR", ["$scope", "$http", function($scope, $http){
+        $http.get('/myReqs').then(function(result){
+           console.log(result); 
+        });
+    }])
+})();
