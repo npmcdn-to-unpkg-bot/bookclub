57d5056c59c5e08a8e0acc5f0160a40b97ffecac
Gibryon Bhojraj <gibryonbhojraj@gmail.com>
2016-02-24 04:03:08 +0000

    Figure out how to display user names and approve requests.

--- app/controllers/myReqs.Client.js
+++ app/controllers/myReqs.Client.js
@@ -4,7 +4,9 @@
     angular.module("myReqs", [])
     .controller("mR", ["$scope", "$http", function($scope, $http){
         $http.get('/myReqs').then(function(result){
-           console.log(result); 
+           console.log(result);
+           $scope.requests = result.data.requests;
+           $scope.loans = result.data.loans;
         });
     }])
 })();
