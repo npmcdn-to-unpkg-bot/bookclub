87f2dbe36f14922aef1e437ebd5d3b3ea854ad2e
Gibryon Bhojraj <gibryonbhojraj@gmail.com>
2016-02-28 21:37:15 +0000

    Back button is reusable.

--- app/controllers/profiles.Client.js
+++ app/controllers/profiles.Client.js
@@ -45,10 +45,17 @@
                    $http.get('https://www.googleapis.com/books/v1/volumes?q=' + x + '+inauthor:' + y).then(function(result){
                        scope.books = result.data.items;
                        console.log(scope.books);
-                       elem.html("<div class='ulcntr'><ul><li class='bkli' ng-repeat='book in books'><a class='bklnks' ng-href='/addbook?title={{book.volumeInfo.title}}&author={{book.volumeInfo.authors | encodeURIComponent}}&tags={{book.volumeInfo.categories | encodeURIComponent}}&cover={{book.volumeInfo.imageLinks.thumbnail | encodeURIComponent}}'><div class='text-center bkinfo' data-toggle='tooltip' title='{{book.volumeInfo.title}}'>{{book.volumeInfo.title}}</div><img class='bkcvr' ng-src={{book.volumeInfo.imageLinks.smallThumbnail}}><div class='bkinfo text-center' data-toggle='tooltip' title='{{book.volumeInfo.authors.join(\", \")}}'>By: <span ng-repeat='author in book.volumeInfo.authors'>{{author}} </span></div></a></li></ul></div><script>$('[data-toggle=tooltip]').hover(function(){console.log('hover'); $(this).tooltip('show');}, function(){$(this).tooltip('hide');})</script>");
+                       elem.html("<div class='ulcntr'><ul><li class='bkli' ng-repeat='book in books'><a class='bklnks' ng-href='/addbook?title={{book.volumeInfo.title}}&author={{book.volumeInfo.authors | encodeURIComponent}}&tags={{book.volumeInfo.categories | encodeURIComponent}}&cover={{book.volumeInfo.imageLinks.thumbnail | encodeURIComponent}}'><div class='text-center bkinfo' data-toggle='tooltip' title='{{book.volumeInfo.title}}'>{{book.volumeInfo.title}}</div><img class='bkcvr' ng-src={{book.volumeInfo.imageLinks.smallThumbnail}}><div class='bkinfo text-center' data-toggle='tooltip' title='{{book.volumeInfo.authors.join(\", \")}}'>By: <span ng-repeat='author in book.volumeInfo.authors'>{{author}} </span></div></a></li></ul><button id='back'>Back</button></div><script>$('[data-toggle=tooltip]').hover(function(){console.log('hover'); $(this).tooltip('show');}, function(){$(this).tooltip('hide');});</script>");
+                       $('#back').click(function(){
+                          elem.html('<add-book-one></add-book-one>');
+                          console.log("Compiling");
+                          $compile(elem.contents())(scope);
+                       });
+                       
                        $compile(elem.contents())(scope);
                    });
                 });
+                
             }
         };
     }])
